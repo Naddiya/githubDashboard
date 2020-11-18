4ab55083392edc4f160b535c45623e99e64686fb
Naddiya <nadiamedkouri@gmail.com>
2020-11-18 13:56:22 +0100

    linter space correction

--- src/components/Page/Error.js
+++ src/components/Page/Error.js
@@ -9,7 +9,7 @@ const Error = () => (
     <Header as="h2">Going nowhere ... </Header>
     {/* <img src="https://media.giphy.com/media/7MnhsCyVGOl56/giphy.gif" alt="guy cycling nowhere" /> */}
     <h3>Erreur 404</h3>
-      <p>Cette page n'existe pas encore ... Clickez pour retrouver la page home</p>
+    <p>Cette page n'existe pas encore ... Clickez pour retrouver la page home</p>
     <Button inverted color="green" as={NavLink} to="/"> Home </Button>
   </Segment>
 );
